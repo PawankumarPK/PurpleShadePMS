d17fb940fb5d0190da51f2e7c1e88b5b75c0de23
PawankumarPK <pk9910765616@gmail.com>
2020-12-14 23:06:28 +0530

    update record successfully

--- controller/ActionsOnRecordApi.js
+++ controller/ActionsOnRecordApi.js
@@ -16,4 +16,31 @@ router.delete("/deleteRecord", function (req, res) {
     })
 })
 
+router.patch("/updateDetails",function(req,res){
+    var id  = req.query.id
+    var title = req.body.title
+    var websiteAddress = req.body.websiteAddress
+    var email = req.body.email
+    var password  = req.body.password
+    var addNote = req.body.addNote
+
+    userRecords.findById(id,function(err,data){
+        data.title = title
+        data.websiteAddress = websiteAddress
+        data.email = email
+        data.password = password
+        data.addNote = addNote
+
+        data.save().then(data =>{
+            res.status(201).json({
+                message : "Record update successfully",
+                result: data
+            })
+        }).catch(err =>{
+            res.json(err)
+        })
+    })
+
+})
+
 module.exports = router
