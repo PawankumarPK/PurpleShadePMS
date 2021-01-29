8b596095b4a00a6d227403a84955700a1874927e
PawankumarPK <pk9910765616@gmail.com>
2021-01-29 21:45:07 +0530

    profile section done

--- controller/ActionsOnRecordApi.js
+++ controller/ActionsOnRecordApi.js
@@ -2,6 +2,8 @@ var express = require("express")
 var router = express()
 
 var userRecords = require("../moduleDB/AddRecordsDB")
+var userSignupRecord = require("../moduleDB/SignupDB")
+
 const { route } = require("./AddRecordsApi")
 
 router.delete("/deleteRecord", function (req, res) {
@@ -14,6 +16,24 @@ router.delete("/deleteRecord", function (req, res) {
         })
     }).catch(err => {
         res.json(err)
+    })
+})
+
+router.patch("/updateProfile", function (req, res) {
+    var id = req.query.id
+    var username = req.body.username
+    var email = req.body.email
+
+    userSignupRecord.findById(id, function (err, data) {
+        data.username = username
+        data.email = email
+
+        data.save().then(data => {
+            res.status(201).json({
+                message: "Profile Update Successfully",
+                result: data
+            })
+        })
     })
 })
 
